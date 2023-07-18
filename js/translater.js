65e6a5f6cec6e830a949e3c4443af4b0f932b6e9
Krish320 <krishyp2@gmail.com>
2023-07-19 00:46:34 +0100

    update

--- js/translater.js
+++ js/translater.js
@@ -5,43 +5,23 @@ import translations_en from "../lng/english.json" assert { type: 'json'};;
 import translations_vi from "../lng/vietnamese.json" assert { type: 'json'};;
 import translations_ro from "../lng/romanian.json" assert { type: 'json' };;
 
-let usrlang;
-if (navigator.userLanguage) // Explorer
-    usrlang = navigator.userLanguage;
-else if (navigator.language) // FF
-    usrlang = navigator.language;
-else
-    usrlang = "en";
-
 window.translate = () => {
 
-    let json_ca = JSON.stringify(translations_ca);
-    let json_ca_Parse = JSON.parse(json_ca);
-
-    let json_vi = JSON.stringify(translations_vi);
-    let json_vi_Parse = JSON.parse(json_vi);
-
-    let json_ro = JSON.stringify(translations_ro);
-    let json_ro_Parse = JSON.parse(json_ro);
-
-    let usr_lng = usrlang.slice(0, 2);
+    let usr_lng = getUserLanguage().slice(0, 2);
 
     if (usr_lng == "ca") {
         setLanguage(translations_ca, "translations_ca");
     }
-
     else if (usr_lng == "ro") {
         setLanguage(translations_ro, "translations_ro");
     }
-
-    else if (usr_lng == "vi") {
+    else if (usr_lng == "de") {
         setLanguage(translations_vi, "translations_vi");
     }
-
     else {
         setLanguage(translations_en, "translations_en");
     }
-};
+}
 
 function translate(json_parse, json_lng) {
     for (let z in json_parse[json_lng][0]) {
@@ -55,4 +35,16 @@ function setLanguage(jsonName, jsonNameString) {
     let json = JSON.stringify(jsonName);
     let jsonParse = JSON.parse(json);
     translate(jsonParse, jsonNameString);
+}
+
+function getUserLanguage() {
+    let usrlang;
+    if (navigator.userLanguage) // Explorer
+        usrlang = navigator.userLanguage;
+    else if (navigator.language) // FF
+        usrlang = navigator.language;
+    else
+        usrlang = "en";
+
+    return usrlang;
 }
